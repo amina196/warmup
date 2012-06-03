1dbc766ef53e4bb428463f1eaffa4abd2991c558
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-06-03 13:26:03 -0400

    changed Readme and clean-up the folders

--- warmup/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Warmup::Application.config.secret_token = '422d9deda6e787f379133dea036b7348008867f2aa7e0d74c150a8ffbf20e7909c9d3c1f4db8076c1557dfe0995f6900ef64b65cb10b846c6dd54647e2c5040d'
